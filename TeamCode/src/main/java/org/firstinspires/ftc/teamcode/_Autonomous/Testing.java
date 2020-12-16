68b5f4455feefa7aed9f1e8a936fce3a23fa22ea
DepressoExpressoBoi <saidajomontano@gmail.com>
2020-12-15 17:30:22 -0700

    AHAHHA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Autonomous/Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Autonomous/Testing.java
@@ -1,3 +1,10 @@
+package org.firstinspires.ftc.teamcode._Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
 @Autonomous
 public class Testing extends LinearOpMode {
     @Override
